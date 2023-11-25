7cf5b31c729a21f0322d39f9bd515431f2873ca7
WL040930 <limweilun3838@gmail.com>
2023-11-25 23:39:00 +0800

    Create login.php
    dsa

---
+++ php/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <!--Make a login page which will read data fron database -- username and password-->
+</body>
+</html>
