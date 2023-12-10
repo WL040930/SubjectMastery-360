41791e871dbec79f9a6a8a66527f522ca14cb162
WL040930 <limweilun3838@gmail.com>
2023-12-10 15:43:03 +0800

    ,

--- php/feedbackbutton.php
+++ php/feedbackbutton.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/feedbackbutton.css">
 </head>
+
 <body>
     <div id="feedback">
-        
+        <a href="feedback.php" id="feedback-link">
+            <img src="../image/feedback.png" alt="Feedback" title="Click Here to Leave Feedback" id="feedback-image">
+        </a>
     </div>
 </body>
 </html>
