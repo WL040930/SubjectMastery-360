c4830df4b5f9286ec51e4244dd73146a02e94a77
WL040930 <limweilun3838@gmail.com>
2023-12-11 08:57:39 +0800

    h

--- php/register.php
+++ php/register.php
@@ -1,88 +1,65 @@
-<?php
-
-    $path = "#";
-    //connection database
-
-    if(isset($_POST['submit'])) {
-        $password = $_POST['password'];
-        $confirm_password = $_POST['confirm_password'];
-        if ($password == $confirm_password) {
-            $path = "feedbackbutton.php";
-        } else {
-            echo "Password not match";
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Your Account</title>
-    <link rel="icon" href="../image/feedback.png">
-    <link rel="stylesheet" href="../css/feedbackbutton.css">
     <link rel="stylesheet" href="../css/register.css">
+    <link rel="icon" href="###"> <!--put icon here-->
 </head>
 
 <body>
-
     <div id="container">
+        <h1>Register Your Acount</h1>
+        <div><p>Register your account here by filling in your information</p></div>
+        <br>
+        <form action="login.php" method="post" onsubmit="return register_validation()">
 
-        <div id="header">
-            <h1 align="center">SUBJECT MASTERY 360</h1>
-            <h2 align="center">SIGN UP</h2>
-        </div>
+            <div class="inp-box">
+                <label for="Username">Username: <font color="red">*</font></label>
+                <input type="text" name="username" id="username" placeholder="Enter Username" required>
+            </div>
 
-        <div id="data-entry">
-            <form action="<?php echo $path; ?>" method="post">
-                <div class="box">
-                    <label for="username">Username: <font color="red">*</font></label>
-                    <input type="text" name="username" id="username" placeholder="Username" required>
-                </div>
-                <div class="box">
-                    <label for="firstname">First Name: <font color="red">*</font></label>
-                    <input type="text" name="firstname" id="firstname" placeholder="First Name" required>
-                </div>
-                <div class="box">
-                    <label for="lastname">Last Name: <font color="red">*</font></label>
-                    <input type="text" name="lastname" id="lastname" placeholder="Last Name" required>
-                </div>
-                <div class="box">
-                    <label for="email">Email Address: <font color="red">*</font></label>
-                    <input type="email" name="email" id="email" placeholder="Email" required>
-                </div>
-                <div class="box">
-                    <label for="password">Password: <font color="red">*</font></label>
-                    <input type="password" name="password" id="password" placeholder="Password" required>
-                </div>
-                <div class="box">
-                    <label for="confirm_password">Confirm Password: <font color="red">*</font></label>
-                    <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" required>
-                </div>
-                <div class="box">
-                    <label for="institute_name">Institute Name: </label>
-                    <input type="text" name="institute_name" id="institute_name" placeholder="Institute Name">
-                </div>
-        </div>
+            <div class="inp-box">
+                <label for="first_name">First Name: <font color="red">*</font></label>
+                <input type="text" name="first_name" id="first_name" placeholder="Enter First Name" required>
+            </div>
 
-        <div id="submit-field">
-                <input type="submit" value="Register" id="submit" name="submit">
-                <div class="login-text">Already have an account? <a href="login.php">Login here.</a>
-            </form>
-        </div>
+            <div class="inp-box">
+                <label for="last_name">Last Name: <font color="red">*</font></label>
+                <input type="text" name="last_name" id="last_name" placeholder="Enter Last Name" required>
+            </div>
 
-        <div id="reset"></div>
+            <div class="inp-box">
+                <label for="email">Email: <font color="red">*</font></label>
+                <input type="email" name="email" id="email" placeholder="Enter Email" required>
+            </div>
 
-        <div id="feedback">
-            <a href="feedback.php" id="feedback-link">
-                <img src="../image/feedback.png" alt="Feedback" title="Click Here to Leave Feedback" id="feedback-image">
-            </a>
-        </div>
+            <div class="inp-box">
+                <label for="institute_name">Institute name: </label>
+                <input type="text" name="institute_name" id="institute_name" placeholder="Enter Institute Name">
+            </div>
 
+            <div class="inp-box">
+                <label for="password">Password: <font color="red">*</font></label>
+                <input type="password" name="password" id="password" placeholder="Enter Password" required>
+            </div>
+
+            <div class="inp-box">
+                <label for="confirm_password">Confirm Password: <font color="red">*</font></label>
+                <input type="password" name="confirm_password" id="confirm_password" placeholder="Enter Confirm Password" required>
+            </div>
+
+            <div class="validate-message"></div>
+            <div class="inp-box">
+                <input type="submit" name="submit" value="Register">
+            </div>
+
+            <div class="inp-box">
+                Already have an account? <a href="login.php">Login here.</a>
+            </div>
+        </form>
     </div>
-
+    <script src="../script/register_validation.js"></script>
 </body>
-
 </html>
