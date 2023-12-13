21d9f96c3c084ddbb0af0a9dcf7481ca921424ea
WL040930 <limweilun3838@gmail.com>
2023-12-13 21:34:40 +0800

    mm

--- php/registration.php
+++ php/registration.php
@@ -1,3 +1,10 @@
+<?php
+
+    include "dbconn.php"; 
+    session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +13,7 @@
     <title>Register Your Account</title>
     <link rel="stylesheet" href="../css/register.css">
     <link rel="stylesheet" href="../css/feedbackbutton.css">
-    <link rel="icon" href="###"> <!--put icon here-->
+    <link rel="icon" href="../image/logo.png"> <!--put icon here-->
 
 </head>
 
@@ -15,26 +22,26 @@
         <h1>Register Your Acount</h1>
         <div><p>Register your account here by filling in your information</p></div>
         <br>
-        <form action="login.php" method="post" onsubmit="return register_validation()">
+        <form action="#" method="post" onsubmit="return register_validation()">
 
             <div class="inp-box">
                 <label for="Username">Username: <font color="red">*</font></label>
-                <input type="text" name="username" id="username" placeholder="Enter Username" required>
+                <input type="text" name="username" id="username" placeholder="Enter Username">
             </div>
 
             <div class="inp-box">
                 <label for="first_name">First Name: <font color="red">*</font></label>
-                <input type="text" name="first_name" id="first_name" placeholder="Enter First Name" required>
+                <input type="text" name="first_name" id="first_name" placeholder="Enter First Name">
             </div>
 
             <div class="inp-box">
                 <label for="last_name">Last Name: <font color="red">*</font></label>
-                <input type="text" name="last_name" id="last_name" placeholder="Enter Last Name" required>
+                <input type="text" name="last_name" id="last_name" placeholder="Enter Last Name">
             </div>
 
             <div class="inp-box">
                 <label for="email">Email: <font color="red">*</font></label>
-                <input type="email" name="email" id="email" placeholder="Enter Email" required>
+                <input type="email" name="email" id="email" placeholder="Enter Email">
             </div>
 
             <div class="inp-box">
@@ -44,12 +51,12 @@
 
             <div class="inp-box">
                 <label for="password">Password: <font color="red">*</font></label>
-                <input type="password" name="password" id="password" placeholder="Enter Password" required>
+                <input type="password" name="password" id="password" placeholder="Enter Password">
             </div>
 
             <div class="inp-box">
                 <label for="confirm_password">Confirm Password: <font color="red">*</font></label>
-                <input type="password" name="confirm_password" id="confirm_password" placeholder="Enter Confirm Password" required>
+                <input type="password" name="confirm_password" id="confirm_password" placeholder="Enter Confirm Password">
             </div>
 
             <div class="validate-message"></div>
@@ -72,3 +79,25 @@
     <script src="../script/register_validation.js"></script>
 </body>
 </html>
+
+<?php
+
+    if(isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $institute_name = $_POST['institute_name'];
+
+        $query = "INSERT INTO `user`(`username`, `user_first_name`, `user_last_name`, `email_address`, `password`, `institute_name`) VALUES ('$username','$first_name','$last_name','$email','$password','$institute_name')";
+        if(mysqli_query($connection, $query)){
+            echo "<script>alert('Registration Successful!')</script>";
+            header(Location: "feedbackbutton.php");
+        } else {
+            echo "<script>alert('Registration Failed!')</script>";
+        }
+        }
+    
+
+?> 
--- script/register_validation.js
+++ script/register_validation.js
@@ -3,38 +3,60 @@ function register_validation() {
     var confirm_password = document.getElementById("confirm_password").value;
     var validate_message = document.getElementsByClassName("validate-message")[0];
     var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var first_name = document.getElementById("first_name").value;
+    var last_name = document.getElementById("last_name").value;
 
     var uppercase = /[A-Z]/;
     var lowercase = /[a-z]/;
     var specialcharacter = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
     var number = /[0-9]/;
     var length = /.{8,}/;
-    var username_length = /.{5,11}/;
+    var username_length = /.{5,11}/;  // Updated the range
+    var name_length = /.{1,15}/;
 
-    if (username.match(username_length)) {
-        if (
-            password.match(uppercase) &&
-            password.match(lowercase) &&
-            password.match(specialcharacter) &&
-            password.match(number) &&
-            password.match(length)
-            ) {
-                if (password == confirm_password) {
-                    validate_message.textContent = "";
-                }
-                else {
-                    validate_message.textContent = "Passwords do not match";
-                    validate_message.style.color = "red"; 
-                    return false;
-            }  
-        } else {
-            validate_message.textContent = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character";
-            validate_message.style.color = "red";
-            return false;
-        }
-    } else {
+    if (!username.match(username_length)) {
         validate_message.textContent = "Username must be between 5 and 11 characters long";
         validate_message.style.color = "red";
         return false;
     }
+
+    if (!first_name.match(name_length)) {
+        validate_message.textContent = "First name must be between 1 and 15 characters long";
+        validate_message.style.color = "red";
+        return false;
+    }
+
+    if (!last_name.match(name_length)) {
+        validate_message.textContent = "Last name must be between 1 and 15 characters long";
+        validate_message.style.color = "red";
+        return false;
+    }
+
+    var passwordRequirements = [
+        uppercase,
+        lowercase,
+        specialcharacter,
+        number,
+        length
+    ];
+
+    for (var i = 0; i < passwordRequirements.length; i++) {
+        if (!password.match(passwordRequirements[i])) {
+            validate_message.textContent = "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character, and be at least 8 characters long";
+            validate_message.style.color = "red";
+            return false;
+        }
+    }
+
+    if (password !== confirm_password) {
+        validate_message.textContent = "Passwords do not match";
+        validate_message.style.color = "red";
+        return false;
+    }
+
+    validate_message.textContent = "";
+    return true;
 }
+
+// add validate email
