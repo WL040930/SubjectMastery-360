873735759ed931038f501481c70c21f177b5fe7d
WL040930 <limweilun3838@gmail.com>
2023-12-14 12:33:52 +0800

    hhi

---
+++ php/admin/admin.php
@@ -0,0 +1,77 @@
+<?php
+
+    include "../dbconn.php"; 
+    session_start();
+    if(isset($_SESSION['admin']) == null) {
+        echo "<script>alert('Authorized Access only!')</script>";
+        echo "<script>window.location.href='../login.php';</script>";
+    } else {
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+    /* Make the ul into menu bar and make it looks nice */
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover:not(.active) {
+        background-color: #111;
+    }
+
+    .active {
+        background-color: #4CAF50;
+    }
+
+    /* split the space between menu section equally */
+    ul {
+        display: flex;
+        justify-content: space-around;
+    }
+    ul li {
+        flex: 1;
+    }
+    ul li a {
+        flex: 1;
+    }
+</style>
+
+
+<body>
+    <div id="header">
+        <div><img src="../../image/logo.png" height="100px"> </div>
+        <div> <h1>Welcome, <?php echo $_SESSION['admin']; }?></h1></div>
+    </div>
+    <br>
+    <ul> 
+        <li><a href="manageuser.php">Manage User</a></li>
+        <li><a href="database.php">Database</a></li>
+        <li><a href="adminfeedback.php">Feedback</a></li>
+        <li><a href="../logout.php">Logout</a></li>
+    </ul>   
+</body>
+</html>
---
+++ php/login.php
@@ -0,0 +1,87 @@
+<?php
+
+    include 'dbconn.php';
+    session_start();
+
+    if(isset($_POST['btnLogin'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $query = "SELECT * FROM `user` WHERE email_address = '$email' AND password = '$password'";
+        $result = mysqli_query($connection, $query); 
+        if(mysqli_num_rows($result) == 1) {
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['user_id'] = $row['id'];
+            $_SESSION['first_name'] = $row['user_first_name'];
+            $_SESSION['last_name'] = $row['user_last_name'];
+            $_SESSION['email'] = $row['email'];
+            if($_SESSION['email'] = "admin@gmail.com") {
+                $_SESSION['admin'] = "admin";
+                header("Location: admin/admin.php");
+            } else {
+                header("Location: index.php");
+            }
+        } else {
+            echo "<script>alert('Invalid email or password');</script>"; 
+        }
+    }
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="icon" href="../image/logo.png">
+</head>
+
+<body>
+    <div id="container">
+        <div id="header">
+            LOGIN TO <br>
+            SUBJECTMASTERY360
+        </div>
+        <div id="input">
+            <form action="" method="post">
+                <table>
+                    <tr>
+                        <td><img src="../image/feedback.png" height="50px"></td>
+                        <td>EMAIL:</td>
+                        <td><input type="email" name="email"></td>
+                    </tr>
+                    <tr>
+                        <td><img src="../image/feedback.png" height="50px"></td>
+                        <td>PASSWORD:</td>
+                        <td><input type="password" name="password"></td>
+                    </tr>
+                    <tr>
+                        <td rolspan="3" align="right">
+                            <input type="submit" value="Login" name="btnLogin">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <div id="footer">
+            <div id="sign-up">
+                Don't have a account? <a href="registration.php">Sign up</a>
+            </div>
+            <div id="forgot-password">
+                <a href="">Forgot Password</a>
+            </div>
+            <div style="clear: both"></div>
+        </div>
+    </div>   
+
+</body>
+</html>
+
+<?php
+
+    mysqli_close($connection);
+
+?>
