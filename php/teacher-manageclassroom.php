f6d8cb5e59d754250498bed8c6b4aadc98e439b2
WL040930 <limweilun3838@gmail.com>
2023-12-28 22:52:13 +0800

    Update teacher-manageclassroom.php

--- php/teacher-manageclassroom.php
+++ php/teacher-manageclassroom.php
@@ -11,6 +11,7 @@
 
         if ($result && mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
+            $color = $row['classroom_color'];
 ?>
 
 <!DOCTYPE html>
@@ -18,151 +19,188 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Manage Classroom</title>
 </head>
 <body>
     <h1>Manage Classroom - <?php echo $row['classroom_name']; ?></h1>
-    <form action="" method="post" enctype="multipart/form-data">
-    <table>
-    <tr>
-        <th>Classroom Name: </th>
-        <td><input type="text" name="name" value="<?php echo $row['classroom_name'];?>"></td>
-    </tr>
-    <tr>
-        <th>Classroom Description: </th>
-        <td><input type="text" name="description" value="<?php echo $row['classroom_description'];?>"></td>
-    </tr>
-    <tr>
-        <th>Classroom Code: </th>
-        <td><input type="text" name="code" value="<?php echo $row['classroom_code'];?>"></td>
-    </tr>
-    <tr>
-        <th>Classroom Picture: </th>
-        <td>
-            <img src="../data/image<?php echo $row['classroom_picture']; ?>" alt="Classroom Picture" width="200px">
-            <br>
-            <input type="file" name="image" id="classroom_picture">
-        </td>
-    </tr>
-    <tr>
-        <th>Classroom Color: </th>
-        <td>
-            Classroom Color : <?php echo $row['classroom_color']; ?>
-            <br>
-            <select name="color" id="color">
-                <option value="AliceBlue">Alice Blue</option>
-                <option value="AntiqueWhite">Antique White</option>
-                <option value="Aqua">Aqua</option>
-                <option value="Aquamarine">Aquamarine</option>
-                <option value="Azure">Azure</option>
-                <option value="Beige">Beige</option>
-                <option value="Bisque">Bisque</option>
-                <option value="BlanchedAlmond">Blanched Almond</option>
-                <option value="BlueViolet">Blue Violet</option>
-                <option value="BurlyWood">Burly Wood</option>
-                <option value="Chartreuse">Chartreuse</option>
-                <option value="Chocolate">Chocolate</option>
-                <option value="Coral">Coral</option>
-                <option value="CornflowerBlue">Cornflower Blue</option>
-                <option value="Cornsilk">Cornsilk</option>
-                <option value="Crimson">Crimson</option>
-                <option value="Cyan">Cyan</option>
-                <option value="FireBrick">Fire Brick</option>
-                <option value="FloralWhite">Floral White</option>
-                <option value="ForestGreen">Forest Green</option>
-                <option value="Gainsboro">Gainsboro</option>
-                <option value="GhostWhite">Ghost White</option>
-                <option value="Gold">Gold</option>
-                <option value="GoldenRod">Golden Rod</option>
-                <option value="Green">Green</option>
-                <option value="GreenYellow">Green Yellow</option>
-                <option value="HoneyDew">Honey Dew</option>
-                <option value="HotPink">Hot Pink</option>
-                <option value="Ivory">Ivory</option>
-                <option value="Khaki">Khaki</option>
-                <option value="Lavender">Lavender</option>
-                <option value="LavenderBlush">Lavender Blush</option>
-                <option value="LawnGreen">Lawn Green</option>
-                <option value="LemonChiffon">Lemon Chiffon</option>
-                <option value="LightBlue">Light Blue</option>
-                <option value="LightCoral">Light Coral</option>
-                <option value="LightCyan">Light Cyan</option>
-                <option value="LightGoldenRodYellow">Light Golden Rod Yellow</option>
-                <option value="LightGray" selected>Light Gray</option>
-                <option value="LightGreen">Light Green</option>
-                <option value="LightPink">Light Pink</option>
-                <option value="LightSalmon">Light Salmon</option>
-                <option value="LightSeaGreen">Light Sea Green</option>
-                <option value="LightSkyBlue">Light Sky Blue</option>
-                <option value="LightSlateGray">Light Slate Gray</option>
-                <option value="LightSteelBlue">Light Steel Blue</option>
-                <option value="LightYellow">Light Yellow</option>
-                <option value="Lime">Lime</option>
-                <option value="LimeGreen">Lime Green</option>
-                <option value="Linen">Linen</option>
-                <option value="Magenta">Magenta</option>
-                <option value="MediumAquaMarine">Medium Aqua Marine</option>
-                <option value="MediumOrchid">Medium Orchid</option>
-                <option value="MediumPurple">Medium Purple</option>
-                <option value="MediumSeaGreen">Medium Sea Green</option>
-                <option value="MediumSlateBlue">Medium Slate Blue</option>
-                <option value="MediumSpringGreen">Medium Spring Green</option>
-                <option value="MediumTurquoise">Medium Turquoise</option>
-                <option value="MediumVioletRed">Medium Violet Red</option>
-                <option value="MintCream">Mint Cream</option>
-                <option value="MistyRose">Misty Rose</option>
-                <option value="Moccasin">Moccasin</option>
-                <option value="NavajoWhite">Navajo White</option>
-                <option value="OldLace">Old Lace</option>
-                <option value="Olive">Olive</option>
-                <option value="OliveDrab">Olive Drab</option>
-                <option value="Orange">Orange</option>
-                <option value="OrangeRed">Orange Red</option>
-                <option value="Orchid">Orchid</option>
-                <option value="PaleGoldenRod">Pale Golden Rod</option>
-                <option value="PaleGreen">Pale Green</option>
-                <option value="PaleTurquoise">Pale Turquoise</option>
-                <option value="PaleVioletRed">Pale Violet Red</option>
-                <option value="PapayaWhip">Papaya Whip</option>
-                <option value="PeachPuff">Peach Puff</option>
-                <option value="Peru">Peru</option>
-                <option value="Pink">Pink</option>
-                <option value="Plum">Plum</option>
-                <option value="PowderBlue">Powder Blue</option>
-                <option value="Red">Red</option>
-                <option value="RosyBrown">Rosy Brown</option>
-                <option value="RoyalBlue">Royal Blue</option>
-                <option value="Salmon">Salmon</option>
-                <option value="SandyBrown">Sandy Brown</option>
-                <option value="SeaGreen">Sea Green</option>
-                <option value="SeaShell">Sea Shell</option>
-                <option value="Sienna">Sienna</option>
-                <option value="Silver">Silver</option>
-                <option value="SkyBlue">Sky Blue</option>
-                <option value="SlateBlue">Slate Blue</option>
-                <option value="SlateGray">Slate Gray</option>
-                <option value="Snow">Snow</option>
-                <option value="SpringGreen">Spring Green</option>
-                <option value="SteelBlue">Steel Blue</option>
-                <option value="Tan">Tan</option>
-                <option value="Teal">Teal</option>
-                <option value="Thistle">Thistle</option>
-                <option value="Tomato">Tomato</option>
-                <option value="Turquoise">Turquoise</option>
-                <option value="Violet">Violet</option>
-                <option value="Wheat">Wheat</option>
-                <option value="White">White</option>
-                <option value="WhiteSmoke">White Smoke</option>
-                <option value="Yellow">Yellow</option>
-                <option value="YellowGreen">Yellow Green</option>
-            </select>
-        </td>
-    </tr>
-</table>
-
+    <form action="" method="post" enctype="multipart/form-data" onsubmit="return validateCode()">
+        <table>
+            <tr>
+                <th>Classroom Name: </th>
+                <td><input type="text" name="name" value="<?php echo $row['classroom_name'];?>"></td>
+            </tr>
+            <tr>
+                <th>Classroom Description: </th>
+                <td><input type="text" name="description" value="<?php echo $row['classroom_description'];?>"></td>
+            </tr>
+            <tr>
+                <th>Classroom Code: </th>
+                <td><input type="text" name="code" id="code" value="<?php echo $row['classroom_code'];?>" onblur="checkCodeAvailability()"></td>
+            </tr>
+            <tr>
+                <th>Classroom Picture: </th>
+                <td>
+                    <img src="../data/image<?php echo $row['classroom_picture']; ?>" alt="Classroom Picture" width="200px">
+                    <br>
+                    <input type="file" name="image" id="classroom_picture">
+                </td>
+            </tr>
+            <tr>
+                <th>Classroom Color: </th>
+                <td>
+                    <select name="color" id="color">
+                        <option value=<?php $row['classroom_color']?>><?php echo $row['classroom_color'];?></option>
+                        <option value="AliceBlue">Alice Blue</option>
+                        <option value="AntiqueWhite">Antique White</option>
+                        <option value="Aqua">Aqua</option>
+                        <option value="Aquamarine">Aquamarine</option>
+                        <option value="Azure">Azure</option>
+                        <option value="Beige">Beige</option>
+                        <option value="Bisque">Bisque</option>
+                        <option value="BlanchedAlmond">Blanched Almond</option>
+                        <option value="BlueViolet">Blue Violet</option>
+                        <option value="BurlyWood">Burly Wood</option>
+                        <option value="Chartreuse">Chartreuse</option>
+                        <option value="Chocolate">Chocolate</option>
+                        <option value="Coral">Coral</option>
+                        <option value="CornflowerBlue">Cornflower Blue</option>
+                        <option value="Cornsilk">Cornsilk</option>
+                        <option value="Crimson">Crimson</option>
+                        <option value="Cyan">Cyan</option>
+                        <option value="FireBrick">Fire Brick</option>
+                        <option value="FloralWhite">Floral White</option>
+                        <option value="ForestGreen">Forest Green</option>
+                        <option value="Gainsboro">Gainsboro</option>
+                        <option value="GhostWhite">Ghost White</option>
+                        <option value="Gold">Gold</option>
+                        <option value="GoldenRod">Golden Rod</option>
+                        <option value="Green">Green</option>
+                        <option value="GreenYellow">Green Yellow</option>
+                        <option value="HoneyDew">Honey Dew</option>
+                        <option value="HotPink">Hot Pink</option>
+                        <option value="Ivory">Ivory</option>
+                        <option value="Khaki">Khaki</option>
+                        <option value="Lavender">Lavender</option>
+                        <option value="LavenderBlush">Lavender Blush</option>
+                        <option value="LawnGreen">Lawn Green</option>
+                        <option value="LemonChiffon">Lemon Chiffon</option>
+                        <option value="LightBlue">Light Blue</option>
+                        <option value="LightCoral">Light Coral</option>
+                        <option value="LightCyan">Light Cyan</option>
+                        <option value="LightGoldenRodYellow">Light Golden Rod Yellow</option>
+                        <option value="LightGray">Light Gray</option>
+                        <option value="LightGreen">Light Green</option>
+                        <option value="LightPink">Light Pink</option>
+                        <option value="LightSalmon">Light Salmon</option>
+                        <option value="LightSeaGreen">Light Sea Green</option>
+                        <option value="LightSkyBlue">Light Sky Blue</option>
+                        <option value="LightSlateGray">Light Slate Gray</option>
+                        <option value="LightSteelBlue">Light Steel Blue</option>
+                        <option value="LightYellow">Light Yellow</option>
+                        <option value="Lime">Lime</option>
+                        <option value="LimeGreen">Lime Green</option>
+                        <option value="Linen">Linen</option>
+                        <option value="Magenta">Magenta</option>
+                        <option value="MediumAquaMarine">Medium Aqua Marine</option>
+                        <option value="MediumOrchid">Medium Orchid</option>
+                        <option value="MediumPurple">Medium Purple</option>
+                        <option value="MediumSeaGreen">Medium Sea Green</option>
+                        <option value="MediumSlateBlue">Medium Slate Blue</option>
+                        <option value="MediumSpringGreen">Medium Spring Green</option>
+                        <option value="MediumTurquoise">Medium Turquoise</option>
+                        <option value="MediumVioletRed">Medium Violet Red</option>
+                        <option value="MintCream">Mint Cream</option>
+                        <option value="MistyRose">Misty Rose</option>
+                        <option value="Moccasin">Moccasin</option>
+                        <option value="NavajoWhite">Navajo White</option>
+                        <option value="OldLace">Old Lace</option>
+                        <option value="Olive">Olive</option>
+                        <option value="OliveDrab">Olive Drab</option>
+                        <option value="Orange">Orange</option>
+                        <option value="OrangeRed">Orange Red</option>
+                        <option value="Orchid">Orchid</option>
+                        <option value="PaleGoldenRod">Pale Golden Rod</option>
+                        <option value="PaleGreen">Pale Green</option>
+                        <option value="PaleTurquoise">Pale Turquoise</option>
+                        <option value="PaleVioletRed">Pale Violet Red</option>
+                        <option value="PapayaWhip">Papaya Whip</option>
+                        <option value="PeachPuff">Peach Puff</option>
+                        <option value="Peru">Peru</option>
+                        <option value="Pink">Pink</option>
+                        <option value="Plum">Plum</option>
+                        <option value="PowderBlue">Powder Blue</option>
+                        <option value="Red">Red</option>
+                        <option value="RosyBrown">Rosy Brown</option>
+                        <option value="RoyalBlue">Royal Blue</option>
+                        <option value="Salmon">Salmon</option>
+                        <option value="SandyBrown">Sandy Brown</option>
+                        <option value="SeaGreen">Sea Green</option>
+                        <option value="SeaShell">Sea Shell</option>
+                        <option value="Sienna">Sienna</option>
+                        <option value="Silver">Silver</option>
+                        <option value="SkyBlue">Sky Blue</option>
+                        <option value="SlateBlue">Slate Blue</option>
+                        <option value="SlateGray">Slate Gray</option>
+                        <option value="Snow">Snow</option>
+                        <option value="SpringGreen">Spring Green</option>
+                        <option value="SteelBlue">Steel Blue</option>
+                        <option value="Tan">Tan</option>
+                        <option value="Teal">Teal</option>
+                        <option value="Thistle">Thistle</option>
+                        <option value="Tomato">Tomato</option>
+                        <option value="Turquoise">Turquoise</option>
+                        <option value="Violet">Violet</option>
+                        <option value="Wheat">Wheat</option>
+                        <option value="White">White</option>
+                        <option value="WhiteSmoke">White Smoke</option>
+                        <option value="Yellow">Yellow</option>
+                        <option value="YellowGreen">Yellow Green</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
+        <div id="validation-message"></div>
+        <input type="submit" value="Update" name="submit">
     </form>
 </body>
 </html>
+
+<script src="../script/teacher-codevalidation.js"></script>
+
+<?php
+
+    if(isset($_POST['submit'])){
+
+    }
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php
