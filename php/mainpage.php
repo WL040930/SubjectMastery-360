49d09f58c87bb4c3a535aedeee35bf2ea3a6237d
North5303 <xtong1120@gmail.com>
2024-01-02 16:17:02 +0800

    cfvghjkl

--- php/mainpage.php
+++ php/mainpage.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - SubjectMaster360</title>
-    <link rel="icon" href="icon.png">
+    <link rel="icon" href="../image/icon.png">
     <link rel="stylesheet" href="../css/mainpage.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
