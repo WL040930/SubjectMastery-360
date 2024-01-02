8b49a1cb995a683197569d116820ba5f25b38add
North5303 <xtong1120@gmail.com>
2024-01-02 15:48:10 +0800

    mainpage

---
+++ php/mainpage.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - SubjectMaster360</title>
+    <link rel="icon" href="icon.png">
+    <link rel="stylesheet" href="mainpage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!--website name font-->
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&display=swap" rel="stylesheet">
+    <!--others font-->
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <div id="logo_menu">
+        <a href="mainpage.php"><img src="pic_re.png" id="weblogo" alt="logo"></a>
+        <div id="webname">
+            <h1>SubjectMaster360</h1>
+        </div>
+        <div id="menubar">
+            <table class="menubar_content">
+                <!--connect to HOME-->
+                <th><a href="mainpage.php" class="menu1">HOME</a></th>
+                <!--connect to ABOUT US-->
+                <th><a href="" class="menu2">ABOUT US</a></th>
+                <!--connect to CUSTOMER FEEDBACK-->
+                <th><a href="" class="menu3">CUSTOMER FEEDBACK</a></th>   
+            </table>
+        </div>
+    </div>
+
+    <div id="all">
+        <div id="slogan">
+            <br>
+            <h1>"Empower Your Mind, Master Every Subject"</h1>
+        </div>
+
+        <div id="login_signup">
+            <div>
+                <table id="login">
+                    <!--connect to LOGIN PAGE-->
+                    <th><a href="https://music.youtube.com/" class="login_button" title="Click to login">LOGIN</a></th>
+                </table>
+            </div>
+            <div>
+                <table id="register">
+                    <!--connect to USER REGISTER PAGE-->
+                    <th><a href="https://youtube.com/" class="register_button" title="Click to create a account">SIGN UP</a></th>
+                </table>
+            </div>
+        </div>
+        <div id="objective">
+            <table id="objective_table">
+                <tr class="obj_img">
+                    <th><img src="pic_re.png" alt="objective1" class="obj1"></th>
+                    <th><img src="pic_re.png" alt="objective2" class="obj2"></th>
+                    <th><img src="pic_re.png" alt="objective3" class="obj3"></th>
+                    <th><img src="pic_re.png" alt="objective4" class="obj4"></th>
+                </tr>
+                <tr class="obj_content">
+                    <td class="con">
+                        <b class="bcon">Facilitate Students Learning Process</b>
+                        <p>SubjectMastery360 will centralize the learning materials that students need to study so that they do not have to spend time searching for them.</p>
+                    </td>
+                    <td class="con">
+                        <b class="bcon">Simplifying Sharing Materials Process</b>
+                        <p>SubjectMastery360 offers “classroom” feature which allows teachers to share the study materials such as notes to their students easily.</p>
+                    </td>
+                    <td class="con">
+                        <b class="bcon">Helps in Analysing & Collecting Data</b>
+                        <p>SubjectMastery360 will automatically generate results graph of the assessment to help teachers in analysing the results of students.</p>
+                    </td>
+                    <td class="con">
+                        <b class="bcon">Optimize processes to conserve time & resources.</b>
+                        <p>SubjectMastery360 will automatically generate the reports of each student and overall report for teachers, which will save the time of teachers in generating and writing the reports. </p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="follow_contact">
+            <div class="fol_title">
+                <h3 class="fol_title_content">FOLLOW US</h3>
+            </div>
+            <div class="fol_content">
+                <div id="follow1">
+                    <img src="pic_re.png" alt="follow our instagram" class="fol_img1">
+                    <p><a href="https://www.instagram.com/" class="fol_con1">@SubjectMaster360</a></p>
+                </div>
+                <br><br>
+                    <div id="follow2">
+                        <img src="pic_re.png" alt="follow our YouTube" class="fol_img2">
+                        <p><a href="https://www.youtube.com/" class="fol_con2">SubjectMaster360</a></p>
+                    </div>
+            </div>
+
+            <div id="contact">
+                <div class="con_title">
+                    <h3 class="con_title_content">CONTACT US</h3>
+                </div>
+                <div class="con_content">
+                    <div id="contact1">
+                        <img src="pic_re.png" alt="contact us through Whatsapp" class="con_img1">
+                        <p><a href="mailto:support@sm360.com" class="con_con1" target="_blank">Email: support@sm360.com</a></p>
+                    </div>
+                    <br><br>
+                        <div id="contact2">
+                            <img src="pic_re.png" alt="contact us through Whatsapp" class="con_img2">
+                            <p><a href="https://wa.link/5df71s" class="con_con2" target="_blank">Tel: +60 16-258 1486</a></p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
