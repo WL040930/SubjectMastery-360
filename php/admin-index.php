fd1e3e62fa1d53a5d0c63eabff403b9879450eb6
WL040930 <limweilun3838@gmail.com>
2024-01-04 23:16:46 +0800

    dd

--- php/admin-index.php
+++ php/admin-index.php
@@ -15,8 +15,25 @@
     <link rel="stylesheet" href="../css/adminmenu.css">
 </head>
 
-
 <body>
- 
+    <?php 
+        $userQuery = "SELECT COUNT(*) AS user_count FROM user;";
+        $userResult = mysqli_query($connection, $userQuery); 
+        if ($userQuery) {
+            $userRow = mysqli_fetch_assoc($userResult);
+    ?>
+    Total User: <?php echo $userRow['user_count'];  ?>
+    <?php } ?>
+
+    <br>
+
+    <?php 
+        $classroomQuery = "SELECT COUNT(*) AS classroom_count FROM classroom;";
+        $classroomResult = mysqli_query($connection, $classroomQuery); 
+        if($classroomQuery){
+            $classroomRow = mysqli_fetch_assoc($classroomResult);
+    ?>
+    Total Classroom: <?php echo $classroomRow['classroom_count']; ?>
+    <?php } ?>
 </body>
 </html>
