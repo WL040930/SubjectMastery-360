a286ee4b42437ff396eb0ba0ffef46d88b23a8e0
North5303 <xtong1120@gmail.com>
2024-01-04 23:33:02 +0800

    admin index done

--- php/admin-index.php
+++ php/admin-index.php
@@ -13,27 +13,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page - Admin</title>
     <link rel="stylesheet" href="../css/adminmenu.css">
+    <!--font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@500&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <?php 
-        $userQuery = "SELECT COUNT(*) AS user_count FROM user;";
-        $userResult = mysqli_query($connection, $userQuery); 
-        if ($userQuery) {
-            $userRow = mysqli_fetch_assoc($userResult);
-    ?>
-    Total User: <?php echo $userRow['user_count'];  ?>
-    <?php } ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User and Classroom Counts</title>
+    <!-- Add your CSS link here -->
+</head>
+<body>
+    <table id="table">
+        <tr>
+            <th id="title">Total User:</th>
+            <td id="content">
+                <?php 
+                    $userQuery = "SELECT COUNT(*) AS user_count FROM user;";
+                    $userResult = mysqli_query($connection, $userQuery); 
+                    if ($userQuery) {
+                        $userRow = mysqli_fetch_assoc($userResult);
+                        echo $userRow['user_count'];
+                    }
+                ?>
+            </td>
+        </tr>
+        <tr>
+            <th id="title">Total Classroom:</th>
+            <td id="content">
+                <?php 
+                    $classroomQuery = "SELECT COUNT(*) AS classroom_count FROM classroom;";
+                    $classroomResult = mysqli_query($connection, $classroomQuery); 
+                    if ($classroomQuery) {
+                        $classroomRow = mysqli_fetch_assoc($classroomResult);
+                        echo $classroomRow['classroom_count'];
+                    }
+                ?>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
 
-    <br>
-
-    <?php 
-        $classroomQuery = "SELECT COUNT(*) AS classroom_count FROM classroom;";
-        $classroomResult = mysqli_query($connection, $classroomQuery); 
-        if($classroomQuery){
-            $classroomRow = mysqli_fetch_assoc($classroomResult);
-    ?>
-    Total Classroom: <?php echo $classroomRow['classroom_count']; ?>
-    <?php } ?>
 </body>
 </html>
