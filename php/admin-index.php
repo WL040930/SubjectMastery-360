ed7d32de1016398edf658831bc5f8c6e0ceb62fc
North5303 <xtong1120@gmail.com>
2024-01-05 00:14:42 +0800

    d

--- php/admin-index.php
+++ php/admin-index.php
@@ -26,13 +26,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User and Classroom Counts</title>
-    <!-- Add your CSS link here -->
+    <!--font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
-    <table id="table">
+    <table>
         <tr>
-            <th id="title">Total User:</th>
-            <td id="content">
+            <th>Total User:</th>
+            <td>
                 <?php 
                     $userQuery = "SELECT COUNT(*) AS user_count FROM user;";
                     $userResult = mysqli_query($connection, $userQuery); 
@@ -44,8 +47,8 @@
             </td>
         </tr>
         <tr>
-            <th id="title">Total Classroom:</th>
-            <td id="content">
+            <th>Total Classroom:</th>
+            <td>
                 <?php 
                     $classroomQuery = "SELECT COUNT(*) AS classroom_count FROM classroom;";
                     $classroomResult = mysqli_query($connection, $classroomQuery); 
