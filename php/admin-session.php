4e47a1692d9c30b65d5b0f791c94ec555886f3d1
WL040930 <limweilun3838@gmail.com>
2024-01-05 10:06:26 +0800

    dd

--- php/admin-session.php
+++ php/admin-session.php
@@ -1,14 +1,19 @@
 <?php
 
+    //check whether the session is started or not
     if(session_status() == PHP_SESSION_NONE) {
         session_start();
     }
 
+    //check whether the user has login or not
     if(!isset($_SESSION['id']) && !isset($_SESSION['role'])) {
         echo "<script> alert('Please Login First.');</script>";
         echo "<script> window.location.href='feature-logout.php' </script>";
     } else {
+        //Get the role from session
         $role = $_SESSION['role'];
+
+        //if the user not admin, then redirect user to logout page
         if($role != 'admin') {
             echo "<script> alert('You are not authorized to view this page.');</script>";
             echo "<script> window.location.href='feature-logout.php' </script>";
@@ -16,3 +21,4 @@
     }
 
 ?>
+
--- php/feature-registration.php
+++ php/feature-registration.php
@@ -73,12 +73,6 @@
         </form>
     </div>
 
-    <div id="feedback">
-        <a href="feedback.php" id="feedback-link">
-            <img src="../image/feedback.png" alt="Feedback" title="Click Here to Leave Feedback" id="feedback-image">
-        </a>
-    </div>
-
     <script src="../script/register_validation.js"></script>
 </body>
 </html>
