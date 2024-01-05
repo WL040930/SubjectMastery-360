247fd0e2b2418f37cbf7a404c8ec387e9e503dd0
North5303 <xtong1120@gmail.com>
2024-01-05 12:30:24 +0800

    d

--- php/stu-teac-editprofile.php
+++ php/stu-teac-editprofile.php
@@ -30,6 +30,7 @@ if (mysqli_num_rows($result) == 1) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Edit Profile</title>
 </head>
 
--- php/stu-teac-editprofilebutton.php
+++ php/stu-teac-editprofilebutton.php
@@ -15,7 +15,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    #edit_button{
+        padding: 10px;
+        margin: 10px;
+        margin-top: 10px;
+        margin-left: 10px;
+        margin-bottom: 10px;
+        font-size: 15px;
+    }
+</style>
 <body>
-    <a href="stu-teac-editprofile.php?id=<?php echo $id; ?>"><button value="Edit Profile">Edit Profile</button></a>
+    <a href="stu-teac-editprofile.php?id=<?php echo $id; ?>"><button id="edit_button" value="Edit Profile">Edit Profile</button></a>
 </body>
 </html>
