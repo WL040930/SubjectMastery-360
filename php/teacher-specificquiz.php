336dd5fd6a58c4aaa52c969a0507fe1b5e5c675d
North5303 <xtong1120@gmail.com>
2024-01-06 12:44:41 +0800

    create classroom

--- php/teacher-specificquiz.php
+++ php/teacher-specificquiz.php
@@ -19,8 +19,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Quiz Result</title>
+    <link rel="icon" href="../image/icon.png">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="../css/teacher-specificquiz.css">
+    <!--font-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@500&display=swap" rel="stylesheet">
