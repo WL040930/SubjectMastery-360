e6e04d84eb4c36eeb245804876b72d8148f4df93
WL040930 <limweilun3838@gmail.com>
2024-01-19 00:39:03 +0800

    dd

--- php/feature-logout.php
+++ php/feature-logout.php
@@ -1,7 +1,10 @@
 <?php
 
     // helps in logging out the user
-    session_start();
+    if(session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    
     session_unset();
     session_destroy();
     header("Location: feature-login.php");
--- php/feature-profilepicture.php
+++ php/feature-profilepicture.php
@@ -167,9 +167,6 @@
             }
         }
     }
-?>
-
-<?php
 
     // Close database connection
     mysqli_close($connection);
