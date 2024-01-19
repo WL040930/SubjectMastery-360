0222e28ec3958ae013587a687370d1ebe2cee6db
WL040930 <limweilun3838@gmail.com>
2024-01-19 18:33:35 +0800

    Update student-viewexam.php

--- php/student-viewexam.php
+++ php/student-viewexam.php
@@ -24,7 +24,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Quiz Result</title>
+    <title>View Exam Result</title>
     <link rel="icon" href="../image/icon.png">
     <link rel="stylesheet" href="../css/animation.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
